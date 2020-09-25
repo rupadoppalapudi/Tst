8c7afb641a97090de96a28903bca964b243fc53e
Rupa Sri <70282754+rupadoppalapudi@users.noreply.github.com>
2020-09-24 21:49:41 -0500

    Add files via upload

---
+++ 2020Fall-Web/Web_ICP5/source/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Todo_timerComponent } from './todo_timer/todo_timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Todo_timerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
